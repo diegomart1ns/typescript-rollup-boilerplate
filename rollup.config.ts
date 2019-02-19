2f40f27ad1a04b5b0dc338ab14291115a7009691
Olavo Parno <olavo.parno@ingresso.com>
2019-02-18 21:47:01 -0300

    chore(rollup): remove terser among other things

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,20 +4,38 @@ import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 import replace from 'rollup-plugin-replace'
-import { terser } from 'rollup-plugin-terser'
 import compiler from '@ampproject/rollup-plugin-closure-compiler'
 
+const canICompile = () => {
+  const spawn = require('child_process').spawn('java', ['-version'])
+  spawn.on('error', () => console.log('Error on spawn'))
+  spawn.stderr.on('data', (data) => {
+      data = data.toString().split('\n')[0]
+      const javaVersion = new RegExp('java version').test(data) ? data.split(' ')[2].replace(/"/g, '') : false
+      if (javaVersion != false) {
+        return compiler()
+      } else {
+        console.log(
+          '\x1b[1m\x1b[31m',
+          'Java is not installed.',
+          'Please do install it in order to run Google Closure Compiler.',
+          'https://www.java.com/en/download/win10.jsp'
+          )
+        return null
+      }
+  })
+}
+
 /**
-* - Defining rollupPlugins
-* - Currently using:
-*  1. Replace (removing node environment things)
-*  2. Resolve (module resolution plugin)
-*  3. JSON (resolve JSON files)
-*  4. TypeScript (compile TS files)
-*  5. CommonJS (allow cjs understanding)
-*  6. Terser (minifies es6+ code)
-*  7. Google Closure Compiler (it reduces the bundle size) 
-*/
+ * - Defining rollupPlugins
+ * - Currently using:
+ *  1. Replace (removing node environment things)
+ *  2. Resolve (module resolution plugin)
+ *  3. JSON (resolve JSON files)
+ *  4. TypeScript (compile TS files)
+ *  5. CommonJS (allow cjs understanding)
+ *  6. Google Closure Compiler (it reduces the bundle size) 
+ */
 const rollupPlugins = [
   replace({
     'process.env.NODE_ENV': JSON.stringify('production')
@@ -28,10 +46,7 @@ const rollupPlugins = [
   // https://github.com/ezolenko/rollup-plugin-typescript2#user-content-plugins-using-asyncawait
   typescript({ useTsconfigDeclarationDir: true, objectHashIgnoreUnknownHack: true }),
   commonjs(),
-  terser({
-    sourcemap: true
-  }),
-  compiler()
+  canICompile(),
 ]
 
 /**
